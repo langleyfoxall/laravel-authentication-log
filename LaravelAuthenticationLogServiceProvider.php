80895602abfb73b64f9926f3b922a4400f094686
Warren Summerton <warren@langleyfoxall.co.uk>
2022-05-18 10:47:31 +0100

    Composer Json written

---
+++ LaravelAuthenticationLogServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LangleyFoxall\LaravelAuthenticationLog;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAuthenticationLogServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        
+    }
+
+    public function boot()
+    {
+        
+    }
+}
