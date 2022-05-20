a6241fe4cdaef2c5a433cf67660ecf5b4b360444
Warren Summerton <warren@langleyfoxall.co.uk>
2022-05-20 13:38:49 +0100

    Submit to PR suggestions

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -9,47 +9,13 @@ class User implements Authenticatable
 {
     public $id;
 
-    /**
-     * Get the name of the unique identifier for the user.
-     *
-     * @return string
-     */
+    // no-op -- method definitions given to comply with Authenticatable Interface,
+    //          purely for testing purposes only
     public function getAuthIdentifierName(){}
-
-    /**
-     * Get the unique identifier for the user.
-     *
-     * @return mixed
-     */
     public function getAuthIdentifier(){}
-
-    /**
-     * Get the password for the user.
-     *
-     * @return string
-     */
     public function getAuthPassword(){}
-
-    /**
-     * Get the token value for the "remember me" session.
-     *
-     * @return string
-     */
     public function getRememberToken(){}
-
-    /**
-     * Set the token value for the "remember me" session.
-     *
-     * @param  string  $value
-     * @return void
-     */
     public function setRememberToken($value){}
-
-    /**
-     * Get the column name for the "remember me" token.
-     *
-     * @return string
-     */
     public function getRememberTokenName(){}
 
 
