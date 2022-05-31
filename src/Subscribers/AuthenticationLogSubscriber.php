8e18776d6d6daeeddac27e92d35ab04af0d1079f
warren-lf <99646743+warren-lf@users.noreply.github.com>
2022-05-31 14:03:37 +0100

    Update src/Subscribers/AuthenticationLogSubscriber.php
    Co-authored-by: Stephen J Sullivan <60765965+SteJaySulli@users.noreply.github.com>

--- src/Subscribers/AuthenticationLogSubscriber.php
+++ src/Subscribers/AuthenticationLogSubscriber.php
@@ -48,7 +48,7 @@ class AuthenticationLogSubscriber
             'authenticatable_type' => get_class($event->user),
             'eventType' => get_class($event),
             'user_ip' => request()->getClientIp(),
-            'recorded_at' => now()
+            'recorded_at' => now(),
         ]);
     }
     public function handleAuthenticatableLockout($event)
